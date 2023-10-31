47f709ac2bb435ce3da9b90a2b63e0dc7c90ed3e
Bao's Laptop <nmtb23@gmail.com>
2023-10-30 21:06:04 -0700

    added auth observer

---
+++ src/lib/AuthObserver.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { firebaseApp, firebaseAuth, firestore } from "$lib/firebaseinit";
+  import { onAuthStateChanged, type User } from "firebase/auth";
+  import { userCred } from "$lib/stores";
+
+  onAuthStateChanged(firebaseAuth, (user) => {
+    if (user) {
+      console.log(`User is signed in as ${user.email}`);
+    }
+  });
+</script>
